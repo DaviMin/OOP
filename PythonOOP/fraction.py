af477acd3ecfc597716382f0463dc43596e4e563
DaviMin <d.minarik@seznam.cz>
2024-04-02 20:28:48 +0200

    solution for fraction task

--- PythonOOP/fraction.py
+++ PythonOOP/fraction.py
@@ -3,3 +3,24 @@ class Fraction:
     def __init__(self, nominator, denominator):
         self.nominator = nominator
         self.denominator = denominator
+
+    def __str__(self):
+        return f"{self.nominator}/{self.denominator}"
+
+    def __eq__(self, other):
+        return self.nominator * other.denominator == self.denominator * other.nominator
+
+    def __add__(self, other):
+        common_denominator = self.denominator * other.denominator
+        new_nominator = self.nominator * other.denominator + other.nominator * self.denominator
+        return Fraction (new_nominator, common_denominator) # vyleze ven objekt, s ekterym budu pracovat v main.py
+
+    def __iadd__(self, other):
+        if isinstance(other, Fraction) # jestli je to spravny datovy typ
+            common_denominator = self.denominator * other.denominator
+            self.nominator = self.nominator * other.denominator + other.nominator * self.denominator
+            self.denominator
+            return self
+
+        raise TypeError("Unsupported operand type for +=: '{}' and '{}'".format(type(self), type(other)))
+    # raise je trochu jine nez try a except, do mainu se vlozi mozne try a except
--- PythonOOP/main.py
+++ PythonOOP/main.py
@@ -1,14 +1,24 @@
-from Fraction import Fraction:
+# Zakladam main, ke kteremu potrebuju objekty zvenci
 
+# ted prenasim funkci Fraction ze souboru fraction.py
+from fraction import Fraction
 
 f1 = Fraction(1,3)
-
 print(f1)
 
 f2 = Fraction(2,3)
+print(f2)
 
 print(f1 == f2)
 
 f3 = f1 + f2
 
 print(f3)
+
+f3 += f2
+print(f3)
+
+try:
+    f3 += 3
+except TypeError:
+    print ("pokusil jsi se secist neshodujici se typy")
